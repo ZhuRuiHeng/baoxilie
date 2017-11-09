081da141e9afbaa50290e2caeaf669f9e6e2f12a
朱瑞恒 <18749830459@163.com>
2017-11-09 20:16:23 +0800

    audio

--- pages/inform/inform.js
+++ pages/inform/inform.js
@@ -20,7 +20,12 @@ Page({
     depintu:false, //拼图是否成功
     clear:true
   },
-
+  onReady: function (e) {
+    // 使用 wx.createAudioContext 获取 audio 上下文 context
+    this.audioCtx = wx.createAudioContext('myAudio')
+    this.audioCtx.setSrc('http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46')
+    // this.audioCtx.play()
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -674,7 +679,7 @@ Page({
   },
   //关闭音乐
   closeAudio(){
-    wx.stopBackgroundAudio()
+    // wx.stopBackgroundAudio()
   },
   // 预览拼图图片
   prewImg() {
@@ -949,5 +954,12 @@ Page({
         // 转发失败
       }
     }
+  },
+  audioPlay: function (e) {
+    console.log(e,11111);
+    this.audioCtx.play()
   }
+  // audioPause: function () {
+  //   this.audioCtx.pause()
+  // }
 })
